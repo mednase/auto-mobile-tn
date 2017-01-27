b246389bc82f1c0d4c5aba24e9a718760a190abc
mednase <mednaceur-marzouk@hotmail.fr>
2017-01-28 00:06:32 +0100

    fix mongose login

--- server/config/params.js
+++ server/config/params.js
@@ -2,7 +2,7 @@ module.exports = {
     secret: 'YouAreMyOnlySecret',
     server_email: 'tuni.env@gmail.com',
     sendgateApi: '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',
-    database: 'mongodb://mednaceur:789456789@ds133279.mlab.com:33279/auto-mobile',
+    database: 'mongodb://mednaceur:War789456789@ds133279.mlab.com:33279/auto-mobile',
     server_url: 'https://auto-mobile-tn.herokuapp.com/',
     itemsPerPage: 12
 };
