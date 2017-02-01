935e334bf5a2ec3f1ba5e7ce9cb8dd8b567c3971
mednase <mednaceur-marzouk@hotmail.fr>
2017-02-01 22:02:06 +0100

    real test

--- public/app/constants.js
+++ public/app/constants.js
@@ -1,6 +1,6 @@
 app.constant('AUTH_EVENTS', {
     notAuthenticated: 'auth-not-authenticated'
 }).constant('API_ENDPOINT', {
-    url: 'http://localhost:8080/api',
-    domain: 'http://localhost:8080/'
+    url: 'https://auto-mobile-tn.herokuapp.com/api',
+    domain: 'https://auto-mobile-tn.herokuapp.com/'
 });
--- server/config/params.js
+++ server/config/params.js
@@ -2,7 +2,7 @@ module.exports = {
     secret: 'YouAreMyOnlySecret',
     server_email: 'tuni.env@gmail.com',
     sendgateApi: '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',
-    database: 'mongodb://localhost:27017/auto_tn',
-    server_url: 'http://localhost:8080/',
+    database: 'mongodb://mednaceur:War789456789@ds157667.mlab.com:57667/tuni-env',
+    server_url: 'https://auto-mobile-tn.herokuapp.com/',
     itemsPerPage: 8
 };
