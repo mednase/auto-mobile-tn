c65409315493f34045a536fac3592b6380b29d78
mednase <mednaceur-marzouk@hotmail.fr>
2018-11-04 20:37:57 +0100

    fix notification

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,8 +2,6 @@
  * Created by medna on 25/01/2017.
  */
 $("document").ready(function () {
-    initNotification();
-
     if ($(window).width() < 1100) {
         $('.page-sidebar-menu').addClass('page-sidebar-menu-closed');
         $('body').toggleClass('page-sidebar-closed');
@@ -45,54 +43,3 @@ $("document").ready(function () {
 
     });
 });
-
-function initNotification() {
-    // Initialize Firebase
-    firebase.initializeApp({
-        "apiKey": "AIzaSyCMdNeXrGpCk2Vm6oV9ArwPDKDnPtX8jdY",
-        "authDomain": "alloambu-fc54b.firebaseapp.com",
-        "databaseURL": "https://alloambu-fc54b.firebaseio.com",
-        "projectId": "alloambu-fc54b",
-        "storageBucket": "alloambu-fc54b.appspot.com",
-        "messagingSenderId": "593992581807"
-    });
-// Retrieve Firebase Messaging object.
-    var messaging = firebase.messaging();
-    navigator.serviceWorker.register('./public/serviceworker.js')
-        .then(function(registration) {
-        messaging.useServiceWorker(registration);
-        messaging.requestPermission().then(function () {
-            if (!isTokenSentToServer())
-                getRegistrationToken();
-        }).catch(function (err) {
-            console.log('Unable to get permission to notify.', err);
-        });
-    });
-
-
-// Get Instance ID token. Initially this makes a network call, once retrieved
-    function getRegistrationToken() {
-        // subsequent calls to getToken will return from cache.
-        messaging.getToken().then(function (currentToken) {
-            if (currentToken) {
-                console.log(currentToken);
-                setTokenSentToServer(currentToken);
-            } else {
-                // Show permission request.
-                console.log('No Instance ID token available. Request permission to generate one.');
-                setTokenSentToServer(false);
-            }
-        }).catch(function (err) {
-            console.log('An error occurred while retrieving token. ', err);
-            setTokenSentToServer(false);
-        });
-    }
-
-    function setTokenSentToServer(sent) {
-        window.localStorage.setItem('sentToServer', sent ? '1' : '0');
-    }
-
-    function isTokenSentToServer() {
-        return window.localStorage.getItem('sentToServer') === '1';
-    }
-}
