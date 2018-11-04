a392b9dd05b167ef9edd0c2b2478329c9348ffef
mednase <mednaceur-marzouk@hotmail.fr>
2018-11-04 21:07:01 +0100

    fix service worker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -130,32 +130,24 @@ self.addEventListener('message', function (evt) {
 
 // The user has clicked on the notification ...
 self.addEventListener('notificationclick', function(event) {
-    console.log('Clicked notification', event);
+    console.log('Clicked notification', event.notification);
     // Android doesn’t close the notification when you click on it
     // See: http://crbug.com/463146
     event.notification.close();
 
-    if(event.notification.data && event.notification.data.click_url) {
-        // gets the notitication click url
-        var click_url = event.notification.data.click_url;
-
-        // This looks to see if the current is already open and
-        // focuses if it is
-        event.waitUntil(clients.matchAll({
-            type: "window"
-        }).then(function(clientList) {
-            for (var i = 0; i < clientList.length; i++) {
-                var client = clientList[i];
-                if (client.url == click_url && 'focus' in client)
-                    return client.focus();
-            }
-            if (clients.openWindow) {
-                var url = click_url;
-                return clients.openWindow(url);
-            }
-
-        }));
-    }
+    // This looks to see if the current is already open and
+    // focuses if it is
+    event.waitUntil(clients.matchAll({
+        type: "window"
+    }).then(function(clientList) {
+        for (var i = 0; i < clientList.length; i++) {
+            var client = clientList[i];
+            if (client.url == '/' && 'focus' in client)
+                return client.focus();
+        }
+        if (clients.openWindow)
+            return clients.openWindow('/');
+    }));
 });
 
 
